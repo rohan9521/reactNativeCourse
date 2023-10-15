2ad1a929e28cc32661c02625dddf10152d3871fd
rohan9521 <rayrohan909@gmail.com>
2023-10-15 11:33:47 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Suprise</Text>
+      <StatusBar style="auto" />
+      <Text>Another text</Text>
+      <Button title="Enter"/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
